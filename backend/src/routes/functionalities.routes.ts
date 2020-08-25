ebc6ac025ad193f6fd18279b3c5dc04985981635
Maykon Sousa <maykon.sousahotmail.com>
2020-08-25 17:03:29 -0300

    create functionality model

--- backend/src/routes/functionalities.routes.ts
+++ backend/src/routes/functionalities.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
 import authentication from '../middlewares/Authentication';
+import ModelFunctionality from '../models/ModelFunctionalities';
 
 import CreateFunctionalityService from '../services/Functionalities/CreateFunctionalitiesService';
 
@@ -19,6 +21,23 @@ functionalitiesRoutes.post('/', async (req, res) => {
     } catch (error) {
         return res.status(400).json({ error: error.message });
     }
+});
+
+// listar funcionalidades
+functionalitiesRoutes.get('/', async (req, res) => {
+    const functionalityRepository = getRepository(ModelFunctionality);
+    const functionalities = await functionalityRepository.find();
+    return res.json(functionalities);
+});
+
+// editar funcionalidades
+functionalitiesRoutes.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, profile } = req.body;
+        const updateFunctionality = new UpdateFunctionalityService();
+        const functionality = await updateFunctionality.execute();
+    } catch (error) {}
 });
 
 export default functionalitiesRoutes;
